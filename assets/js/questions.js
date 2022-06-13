f52fe137482adda33ff2c3df19881b8c1b4474ca
Sanyiah Zulfiqar <sanyiahzulfiqar1996@gmail.com>
2022-06-13 14:48:53 +0100

    adding questions and event listener to start button

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+let questions = [
+    {
+        title: "What does JS stand for in relation to this coding course?",
+        choices: ["Just Saying", "Java Script", "Java Shava", "Jay Sean"],
+        answer: "Java Script"
+    },
+    {
+        title: "What does CSS stand for?",
+        choices: ["Common Style Sheet", "Colourful Style Sheet", "Cascading Style Sheet", "Colourful Styling Sheet"],
+        answer: "Cascading Styling Sheet"
+    },
+    {
+        title: "Which of the following is NOT a JS data type?",
+        choices: ["Boolean", "Thread", "String", "Undefined"],
+        answer: "Thread"
+    },
+    {
+        title: "Which of the following is classified as a boolean value?",
+        choices: ["Yes", "No", "Maybe", "False"],
+        answer: "False"
+    },
+    {
+        title: "What does HTML stand for?",
+        choices: ["Hyper Text Preprocessor", "Hyper Text Markup Language",  "Hyper Text Multiple Language", "Hyper Tool Multi Language"],
+        answer: "Hyper Text Markup Language"
+    }
+]
